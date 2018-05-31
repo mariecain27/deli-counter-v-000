be9779af02380e65f005232abeb7862f262b676a
Marie Cain <mariecain27@gmail.com>
2018-05-31 02:43:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty"
   else
-    #shows everyone their current place in line
+    # puts "The line is currently: 1.Ada 2.Grace 3.Kent"
     false
   end
 end
