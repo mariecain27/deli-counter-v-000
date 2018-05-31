73abfcc953c2a010412663d995ce95f9c4003ebf
Marie Cain <mariecain27@gmail.com>
2018-05-31 03:01:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,10 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    counter = 0
     katz_deli.each do |name, counter|
-      counter += 1
+      counter = 0
       temporary_array.push("#{counter}. #{name}")
+      counter += 1
     end
     puts "The line is currently: #{temporary_array.join(" ")}"
   end
