947f96223bc31c1013edd1a1639f8aa97471f52e
Marie Cain <mariecain27@gmail.com>
2018-05-31 03:23:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  #puts "Currently serving #{name}."
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{name}."
+    
 end
