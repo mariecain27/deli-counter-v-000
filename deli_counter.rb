2f839fc8f9662ee4e0b7cad9181a7a2b760728ad
Marie Cain <mariecain27@gmail.com>
2018-05-31 02:39:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty"
+  else
+    #shows everyone their current place in line
+    false
+  end
+end
